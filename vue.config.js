3b4da8b93dd9ef60878ff1efb195eebf51f91256
hu <3195843964@qq.com>
2024-09-19 00:06:11 +0800

    Initial

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/my-vue-app-for-baobao/'  // 替换为你的 GitHub 仓库名
+    : '/'
 })
